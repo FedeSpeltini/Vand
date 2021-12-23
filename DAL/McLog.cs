42197a72fa1dba0d0354bd30bd1291949e0c6eae
Federico Speltini <fedesp71@gmail.com>
2021-12-23 15:39:55 -0300

    add log system

---
+++ DAL/McLog.cs
@@ -0,0 +1,91 @@
+﻿using Abstractions;
+using BE;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class McLog
+    {
+
+        public static void CreateLog(ILog log)
+        {
+            Acceso.Abrir();
+
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+            parameters.Add(Acceso.CrearParametro("@Nombre", log.Usuario.Nombre));
+            parameters.Add(Acceso.CrearParametro("@FecIngreso", log.FecIngreso));
+            Acceso.Escribir("spCrearLog", parameters);
+
+            Acceso.Cerrar();
+        }
+
+        public static List<ILog> GetLogs()
+        {
+            List<ILog> tablaLogs = new List<ILog>();
+            Acceso.Abrir();
+
+            DataTable tabla = Acceso.Leer("spListarCopias");
+            Acceso.Cerrar();
+
+            foreach (DataRow registro in tabla.Rows)
+            {
+                Log log = new Log();
+                UsuarioEntity usuario = new UsuarioEntity();
+                usuario.Nombre = registro["fecIngreso"].ToString();
+                log.Usuario = usuario;
+                log.FecIngreso = DateTime.Parse(registro["fecIngreso"].ToString());
+                if (registro["fecEgreso"] != null)
+                {
+                    log.FecEgreso = DateTime.Parse(registro["fecEgreso"].ToString());
+                }
+                tablaLogs.Add(log);
+            }
+            return tablaLogs;
+        }
+
+        public static List<ILog> GetLog(IUsuario usuario)
+        {
+            List<ILog> tablaLogs = new List<ILog>();        
+            Acceso.Abrir();
+
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+            parameters.Add(Acceso.CrearParametro("@Nombre", usuario.Nombre));
+            
+            DataTable tabla = Acceso.Leer("spListarCopias", parameters);
+            Acceso.Cerrar();
+
+            foreach (DataRow registro in tabla.Rows)
+            {
+                Log log = new Log();
+
+                log.Usuario = usuario;
+                log.FecIngreso = DateTime.Parse(registro["fecIngreso"].ToString());
+                if(registro["fecEgreso"] != null)
+                {
+                    log.FecEgreso = DateTime.Parse(registro["fecEgreso"].ToString());
+                }
+                tablaLogs.Add(log);
+            }
+            return tablaLogs;
+        }
+
+        public static void PutFinishHourLog(ILog log)
+        {
+            Acceso.Abrir();
+
+            List<IDbDataParameter> parameters = new List<IDbDataParameter>();
+            parameters.Add(Acceso.CrearParametro("@Nombre", log.Usuario.Nombre));
+            parameters.Add(Acceso.CrearParametro("@FecIngreso", log.FecIngreso));
+            parameters.Add(Acceso.CrearParametro("@FecEgreso", log.FecEgreso));
+            Acceso.Escribir("spPutFinishHourLog", parameters);
+
+            Acceso.Cerrar();
+        }
+
+    }
+}
---
+++ Views/FrmLogs.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Views
+{
+    partial class FrmLogs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtLogs = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtLogs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtLogs
+            // 
+            this.dtLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtLogs.Location = new System.Drawing.Point(297, 32);
+            this.dtLogs.Name = "dtLogs";
+            this.dtLogs.RowHeadersWidth = 51;
+            this.dtLogs.RowTemplate.Height = 24;
+            this.dtLogs.Size = new System.Drawing.Size(477, 371);
+            this.dtLogs.TabIndex = 3;
+            // 
+            // FrmLogs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtLogs);
+            this.Name = "FrmLogs";
+            this.Text = "FrmLogs";
+            ((System.ComponentModel.ISupportInitialize)(this.dtLogs)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtLogs;
+    }
+}
